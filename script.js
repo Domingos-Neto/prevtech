4463b5de4cb5147a72b7976b35a7a0a429e8af7f
dtechinformatica <domingosbarroson@gmail.com>
2025-09-10 20:22:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3014,8 +3014,9 @@ function gerarDeclaracaoNaoAcumulacao() {
             <p>INSTITUTO DE PREVIDÊNCIA DOS SERVIDORES MUNICIPAIS DE ITAPIPOCA</p>
         </div>
         <h2>DECLARAÇÃO DE NÃO ACUMULAÇÃO DE CARGOS</h2>
-        <p>Eu, <b>${dadosServidor.nomeServidor || '_________________________'}</b>, portador(a) do RG nº <b>${dadosServidor.rgServidor || '________________'}</b> e inscrito(a) no CPF sob o nº <b>${dadosServidor.cpfServidor || '________________'}</b>, matrícula nº <b>${dadosServidor.matriculaServidor || '________________'}</b>, residente e domiciliado(a) em ____________________________________________________________________, declaro, para os devidos fins de direito, que não exerço acumulativamente nenhum outro cargo ou função pública, em conformidade com o princípio constitucional previsto no Art. 37, XVI e XVII, da Constituição da República Federativa do Brasil e em observância às normas estabelecidas no Estatuto dos Servidores Públicos do Município de Itapipoca. [cite: 4, 6, 9, 10]</p>
-        <p>E por ser a expressão da verdade, firmo a presente para que surta seus efeitos legais. [cite: 11]</p>
+        <p>
+            Eu, <b>${dadosServidor.nomeServidor || '_________________________'}</b>, portador(a) do RG nº <b>${dadosServidor.rgServidor || '________________'}</b> e inscrito(a) no CPF sob o nº <b>${dadosServidor.cpfServidor || '________________'}</b>, matrícula nº <b>${dadosServidor.matriculaServidor || '________________'}</b>, residente e domiciliado(a) em <b>${dadosServidor.enderecoServidor || '________________________________________'}</b>, declaro, para os devidos fins de direito, que não exerço acumulativamente nenhum outro cargo ou função pública, em conformidade com o princípio constitucional previsto no Art. 37, XVI e XVII, da Constituição da República Federativa do Brasil e em observância às normas estabelecidas no Estatuto dos Servidores Públicos do Município de Itapipoca. E por ser a expressão da verdade, firmo a presente para que surta seus efeitos legais.
+        </p>
         <p style="margin-top: 40px;">Itapipoca - CE, ${dataFormatada}.</p>
         <div class="signature-block">
             <div class="signature-line"></div>
@@ -3352,6 +3353,7 @@ Object.assign(window, {
 });
 
 window.simulacao = simulacao;
+
 
 
 
